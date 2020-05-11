a1906f905a0a5fb8478c5095b4980a9f2ec7de75
Nuran-Gahwagi <nuranzg@gmail.com>
2020-05-11 16:47:05 +0200

    l

---
+++ myproject.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+
+
+$("#indi").click(function(){
+	$(".imgAme").hide()
+	$(".imgindi").show()
+	$(".imgDub").hide()
+});
+ $("#amer").click(function(){
+	$(".imgindi").hide()
+	$(".imgAme").show()
+	$(".imgDub").hide()
+});
+ $("#Dub").click(function(){
+	$(".imgindi").hide()
+	$(".imgAme").hide()
+	$(".imgDub").show()
+});
+$("#tog").click(function(){
+	$(".imgindi").show()
+	$(".imgAme").show()
+	$(".imgDub").show()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});

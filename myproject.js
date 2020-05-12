24c1f1051d8b539a20ace87021f9d912327fd06c
Nuran-Gahwagi <nuranzg@gmail.com>
2020-05-12 03:00:42 +0200

    4

--- myproject.js
+++ myproject.js
@@ -3,16 +3,19 @@ $("#indi").on("click", function(){
 	$(".box3").hide()
 	$(".box").show()
 	$(".box2").hide()
+	$(".par").html("<p>" + para[0] + "</p>")
 });
  $("#amer").on("click", function(){
 	$(".box3").hide()
 	$(".box").hide()
 	$(".box2").show()
+	$(".par").html("<p>" + para[1] + "</p>")
 });
 $("#dub").on("click", function(){
 	$(".box3").show()
 	$(".box").hide()
 	$(".box2").hide()
+	$(".par").html("<p>" + para[2] + "</p>")
 });
 $("#tog").on("click", function(){
 	$(".box3").show()
@@ -22,31 +25,8 @@ $("#tog").on("click", function(){
 
 
 
-var para = ["Taj Mahal An immense mausoleum of white marble built in Agra between 1631 and 1648 by order of the Mughal emperor Shah Jahan in memory of his favourite wife, the Taj Mahal is the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage.", "The motto on the Great Seal of the United States sums up the country. It means out of many, one. Not only that, but the motto is in Latin, a language that not one American ever spoke natively! That's America. The United States is made up of countless parts, most of them from other places. "
-, "Indulgent, glamorous, and progressive are words that describe Dubai. This United Arab Emirates city is a luxury travel destination for leisure and business travellers. Dubai combines a modern metropolis with a timeless sensibility and Arabian flair."]
- for(i = 0 ;i < para.length;i++){
-                $('.par').append('<p>'+ para[i]+'</p>');
-            }
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
+var para = ["Taj Mahal An immense mausoleum of white marble built in Agra between 1631 and 1648 by order of the Mughal emperor Shah Jahan in memory of his favourite wife, the Taj Mahal is the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage.", "Times Square is a section of Manhattan, New York City. It is a major center for tourism, show business and commerce. The square is at the meeting point of Broadway, 7th Avenue and 42nd street. It is named for the New York Times which formerly had its main building there. "
+, " In a city that is full of awe striking buildings, Dubai Marina stands out as the home to some of Dubai newest and most fascinating buildings and attractions where visitors can relax in comfort and luxury along the waterfront, Indulgent, glamorous, and progressive are words that describe Dubai. This United Arab Emirates city is a luxury travel destination for leisure and business travellers. Dubai combines a modern metropolis with a timeless sensibility and Arabian flair."]
 
 
 

9c2a3852613febc461d951734e14e4cafd89b5a1
Nuran-Gahwagi <nuranzg@gmail.com>
2020-05-12 12:07:56 +0200

    l

--- myproject.js
+++ myproject.js
@@ -4,6 +4,7 @@ $("#indi").on("click", function(){
 	$(".box").show()
 	$(".box2").hide()
 	$(".par").html("<p>" + para[0] + "</p>")
+	//$(".down").html("<p>" + key + ":"+ value + "</p>")
 });
  $("#amer").on("click", function(){
 	$(".box3").hide()
@@ -16,6 +17,7 @@ $("#dub").on("click", function(){
 	$(".box").hide()
 	$(".box2").hide()
 	$(".par").html("<p>" + para[2] + "</p>")
+ 
 });
 $("#tog").on("click", function(){
 	$(".box3").show()
@@ -28,8 +30,31 @@ $("#tog").on("click", function(){
 var para = ["Taj Mahal An immense mausoleum of white marble built in Agra between 1631 and 1648 by order of the Mughal emperor Shah Jahan in memory of his favourite wife, the Taj Mahal is the jewel of Muslim art in India and one of the universally admired masterpieces of the world's heritage.", "Times Square is a section of Manhattan, New York City. It is a major center for tourism, show business and commerce. The square is at the meeting point of Broadway, 7th Avenue and 42nd street. It is named for the New York Times which formerly had its main building there. "
 , " In a city that is full of awe striking buildings, Dubai Marina stands out as the home to some of Dubai newest and most fascinating buildings and attractions where visitors can relax in comfort and luxury along the waterfront, Indulgent, glamorous, and progressive are words that describe Dubai. This United Arab Emirates city is a luxury travel destination for leisure and business travellers. Dubai combines a modern metropolis with a timeless sensibility and Arabian flair."]
 
+var obj = {
+	Location1:"Taj Mahal, Agar",
+	Location2:"Times square, NewYork",
+	Location3:"Marina, Dubai"
+};
 
+$( "#inputA" ).on( "blur", function() {
+    alert( "Please select a place!" );
+  
+});
+$('#og').click(function() {
 
+   $('.down1').append('<p>' + obj.Location1 + '</p>');
+   $('.down2').append('<p>' + obj.Location2 + '</p>');
+   $('.down3').append('<p>' + obj.Location3 + '</p>');
+})
+
+/*$.each(obj, function(key, value){
+	(key + ":" + value);
+});
+
+$.when( { testing: "opening new tab" } ).done(function( x ) {
+  alert( x.testing ); 
+});
+*/
 
 
 
